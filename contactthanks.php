b8f2289e5ac1a30f54c6966c2dfcd4c92cdecc33
dwysefisher <wysefisher@gmail.com>
2013-12-23 14:16:36 -0600

    origin

---
+++ contactthanks.php
@@ -0,0 +1,40 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Strict//EN">
+
+<head>
+	<title>Have questions? Contact Barrick-Oltman</title>
+	
+	<link rel="stylesheet" type="text/css" href="css/contact_style.css" />
+    <link href="css/styles.css" rel="stylesheet" type="text/css" />
+<link rel="stylesheet" href="css/page.css" type="text/css" media="screen" />
+</head>
+
+<body>
+
+
+
+	<div id="page-wrap">
+
+		<!--<img src="images/title.gif" alt="A Nice & Simple Contact Form" />
+		<p>By <a href="http://css-tricks.com">CSS-Tricks</a></p>-->
+			
+			
+		<br /><br />
+		
+			
+		<h2>Your message has been sent and we will contact you shortly!</h2><br />
+		
+		<p><a href="index.html">Back to Barrick-Oltman.com</a></p>
+	
+	</div>
+    
+	<!--
+	<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
+	</script>
+	<script type="text/javascript">
+	_uacct = "UA-68528-29";
+	urchinTracker();
+	</script> -->
+
+</body>
+
+</html>
